a03ba566090311a5a94fbd1628365f35df41d0b2
Gad <objetunique@gmail.com>
2025-04-05 13:55:13 +0200

    refactoring

--- main.go
+++ main.go
@@ -14,98 +14,16 @@ import (
 )
 
 type arc string
+
+// Handler of each arc/chapter. implements ServeHTTP Handler interface
 type ArcHandler struct {
 	arc arc
 }
-var tmplFile = "templates/arc.tmpl"
-var htmlTempDir = "tmp/html"
 
-
-func main() {
-
-	f, err := os.Open("gopher.json")
-	if err != nil {
-		log.Panicln(err.Error())
-	}
-
-	st, err := io.ReadAll(f)
-	if err != nil {
-		log.Println(err.Error())
-	}
-
-	type arcContent struct {
-		Title   string   `json:"title"`
-		Story   []string `json:"story"`
-		Options []struct {
-			Text string `json:"text"`
-			Arc  string `json:"arc"`
-		} `json:"options"`
-	}
-
-	chapters := map[arc]arcContent{}
-
-	err = json.Unmarshal(st, &chapters)
-	if err != nil {
-		log.Panicln(err.Error())
-	}
-
-	tmpl, err := template.ParseFiles(tmplFile)
-	if err != nil {
-		panic(err)
-	}
-
-	for arc := range chapters {
-
-		fileName := fmt.Sprintf("%s/arc_%s.html", htmlTempDir, arc)
-
-		f, err = os.Create(fileName)
-		if err != nil {
-			panic(err)
-		}
-		defer f.Close()
-
-		err = tmpl.Execute(f, chapters[arc])
-		if err != nil {
-			panic(err)
-		}
-	}
-	for arc := range chapters {
-		archHandler := &ArcHandler{
-			arc: arc,
-		}
-		path := fmt.Sprintf("/%s", arc)
-		http.Handle(path, archHandler)
-	}
-
-	go func() {
-		if err := http.ListenAndServe(":8000", nil); err != http.ErrServerClosed {
-			log.Fatalf("Server error : %v", err)
-		}
-
-	}()
-	// handle graceful shutdown
-	stop := make(chan os.Signal, 1)
-	signal.Notify(stop, os.Interrupt, syscall.SIGTERM)
-	<-stop
-
-	// cleaning temporary html files
-	
-	globPattern := fmt.Sprintf("%s/*.html",htmlTempDir)
-	matches, err := filepath.Glob(globPattern)
-	if err!=nil {
-		log.Println(err)
-	}
-	for _,m := range matches {
-		if err = os.Remove(m); err != nil {
-			log.Fatalf("Error cleaning temporary html files : %v", err)
-		}
-
-	}
-
-}
-
+// pour the html file corresponding to each handler into the http paquet
 func (s *ArcHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 
+	
 	htmlPath := fmt.Sprintf("%s/arc_%s.html", htmlTempDir, s.arc)
 	f, err := os.Open(htmlPath)
 	if err != nil {
@@ -118,3 +36,139 @@ func (s *ArcHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	}
 	w.Write([]byte(st))
 }
+
+
+const (
+	tmplFile    = "templates/arc.tmpl"
+	htmlTempDir = "tmp/html"
+)
+
+type arcContent struct {
+	Title   string   `json:"title"`
+	Story   []string `json:"story"`
+	Options []struct {
+		Text string `json:"text"`
+		Arc  string `json:"arc"`
+	} `json:"options"`
+}
+
+
+// parse json file into a map of arcContent
+func storyParsing(fileName string) (chapters map[arc]arcContent, err error) {
+
+	f, err := os.Open(fileName)
+	if err != nil {
+		return nil, err
+	}
+
+	st, err := io.ReadAll(f)
+	if err != nil {
+		return nil, err
+	}
+
+	chapters = map[arc]arcContent{}
+
+	err = json.Unmarshal(st, &chapters)
+	if err != nil {
+		return nil, err
+	}
+
+	return chapters, nil
+}
+
+// generate html files for each chapter based on a template file
+func htmlGenerate(chapters map[arc]arcContent, tmplFile string, htmlTempDir string) error {
+
+	// 1- creates a new template from tmplFile
+	// 2- creates as many html file as chapters in htmlTempDir
+	// 3- applies the template to each chapter to create the html and save them into files
+
+	for arc := range chapters {
+
+		tmpl, err := template.ParseFiles(tmplFile)
+		if err != nil {
+			panic(err)
+		}
+
+		fileName := fmt.Sprintf("%s/arc_%s.html", htmlTempDir, arc)
+
+		f, err := os.Create(fileName)
+		if err != nil {
+			return err
+		}
+		defer f.Close()
+
+		err = tmpl.Execute(f, chapters[arc])
+		if err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
+
+// create a new endpoint for each chapter and register the handler
+func registerHandlers(chapters map[arc]arcContent) {
+
+	
+	for arc := range chapters {
+		archHandler := &ArcHandler{
+			arc: arc,
+		}
+		path := fmt.Sprintf("/%s", arc)
+		http.Handle(path, archHandler)
+	}
+}
+
+// cleans the html temp dir
+func cleaning(htmlTempDir string) error {
+	globPattern := fmt.Sprintf("%s/*.html", htmlTempDir)
+	matches, err := filepath.Glob(globPattern)
+	if err != nil {
+		return err
+	}
+	for _, m := range matches {
+		if err = os.Remove(m); err != nil {
+			return err
+		}
+
+	}
+	return nil
+}
+
+
+
+func main() {
+
+
+	chapters, err := storyParsing("gopher.json")
+	if err != nil {
+		log.Panic(err)
+	}
+
+	err = htmlGenerate(chapters, tmplFile, htmlTempDir)
+	if err != nil {
+		log.Panic(err)
+	}
+
+	registerHandlers(chapters)
+
+	// run http.ListenAndServe in a go routine to manage graceful shutdown
+	go func() {
+		if err := http.ListenAndServe(":8000", nil); err != http.ErrServerClosed {
+			log.Fatalf("Server error : %v", err)
+		}
+
+	}()
+
+	// handle graceful shutdown
+	stop := make(chan os.Signal, 1)
+	signal.Notify(stop, os.Interrupt, syscall.SIGTERM)
+	<-stop
+
+	// cleaning temporary html files
+	if err = cleaning(htmlTempDir); err != nil {
+		log.Fatalf("Error cleaning temporary html files : %v", err)
+	}
+}
+
